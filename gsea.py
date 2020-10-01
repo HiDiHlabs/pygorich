02a1b7bc205c4ed0079776e68cd6c526c85d1c3f
biegm <matthias.bieg@charite.de>
2020-10-01 16:28:20 +0200

    Finish first working implementation of region to pathway enrichment analysis given a foregoumd, and background of genomic regions.

--- gsea.py
+++ gsea.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import pandas as pnd
+from scipy.stats import hypergeom, fisher_exact, binom_test
+from statsmodels.stats.multitest import multipletests
 import sys
 sys.path += ["/home/biegm/projects/python_packages"]
 import pyanno
@@ -47,6 +49,16 @@ class Enricher():
         Loads annotation database used for annotating foregound and
         background regions.
 
+    loadRegions(self,
+               foreground_bed_filename = None,
+               background_bed_filename = None)
+        Load and annotate foreground and background genomic regions.
+
+    loadLibrary(library_id,
+                library_filename=None,
+                from_enrichr=True)
+        Load genesets from library.
+
     getAnnotationDatabase()
         Returns annotation database.
 
@@ -80,6 +92,21 @@ class Enricher():
         self.__load_genesets_from_enrichr()
 
         self.__annotation_database = None
+
+        self.__enrichment_results = pnd.DataFrame(columns = 
+                                                  ["LIB.ID",
+                                                   "GENESET.ID",
+                                                   "n.FOREGROUND",
+                                                   "n.BACKGROUND",
+                                                   "n.FOREGROUND.IN.SET",
+                                                   "n.BACKGROUND.IN.SET",
+                                                   "p.FISHER",
+                                                   "q.FISHER",
+                                                   "odds.FISHER",
+                                                   "p.HYPERGEOMETRIC",
+                                                   "q.HYPERGEOMETRIC",
+                                                   "p.BINOMIAL",
+                                                   "q.BINOMIAL"])
 
     ################
     # Public Methods
@@ -127,6 +154,44 @@ class Enricher():
             DataFrame storing loaded genesets
         '''
         return self.__genesets
+
+    def getEnrichmentResults(self):
+        '''Returns enrichment results.
+        Returns
+        -------
+        enrichment_results : pandas.DataFrame
+            DataFrame storing enrichment results.
+        '''
+        return self.__enrichment_results
+
+    def getLibraryIDs(self):
+        '''Return Library IDs
+        Returns
+        -------
+        library_ids : list
+            List of library IDs
+        '''
+        return self.__library_ids
+
+    # Setter methods
+    def resetEnrichmentResults(self):
+        '''Reset enrichment results to empty DataFrame.
+        '''
+        self.__enrichment_results = pnd.DataFrame(columns = 
+                                                  ["LIB.ID",
+                                                   "GENESET.ID",
+                                                   "n.FOREGROUND",
+                                                   "n.BACKGROUND",
+                                                   "n.FOREGROUND.IN.SET",
+                                                   "n.BACKGROUND.IN.SET",
+                                                   "p.FISHER",
+                                                   "q.FISHER",
+                                                   "odds.FISHER",
+                                                   "p.HYPERGEOMETRIC",
+                                                   "q.HYPERGEOMETRIC",
+                                                   "p.BINOMIAL",
+                                                   "q.BINOMIAL"])
+
 
     # Data load Methods
     def loadAnnotationDatabase(self,
@@ -186,7 +251,7 @@ class Enricher():
     def loadRegions(self,
                     foreground_bed_filename = None,
                     background_bed_filename = None):
-        '''Load and annotate foreground and background genomic regions
+        '''Load and annotate foreground and background genomic regions.
         Parameters
         ----------
         foreground_bed_filename : string
@@ -194,7 +259,7 @@ class Enricher():
             contain header that starts with "#chrom start end".
         background_bed_filename : string
             Path to bed file containing background regions. Important: Must
-            contain header that starts with "#chrom start end"
+            contain header that starts with "#chrom start end".
         '''
         # Load and annotated foreground regions
         # Create a new GenomicRegionAnnotator instance
@@ -212,6 +277,12 @@ class Enricher():
         # Retrieve annotated base intervals as pandas.DataFrame instance
         self.__annotated_foreground = gra.get_base()
 
+        self.__annotated_foreground.loc[:, "GENESET"] = [
+        ";".join([ gene.split("(")[0] for gene in row["enhancer"].split(";") ]) 
+        if row["enhancer"] != "NA" 
+        else row["genes"].split(";")[0].split("(")[0] 
+        for i, row in self.__annotated_foreground.iterrows()]
+
         # Load and annotated background regions
         # Create a new GenomicRegionAnnotator instance
         gra = pyanno.Annotator.GenomicRegionAnnotator()
@@ -228,6 +299,198 @@ class Enricher():
         # Retrieve annotated base intervals as pandas.DataFrame instance
         self.__annotated_background = gra.get_base()
 
+        self.__annotated_background.loc[:, "GENESET"] = [
+        ";".join([ gene.split("(")[0] for gene in row["enhancer"].split(";") ])
+        if row["enhancer"] != "NA" 
+        else row["genes"].split(";")[0].split("(")[0] 
+        for i, row in self.__annotated_background.iterrows()]
+
+    def loadLibrary(self,
+                    library_id,
+                    library_filename=None,
+                    from_enrichr=True):
+        '''Load genesets from library
+        Parameters
+        ----------
+        library_id : string
+            Identifier of geneset library.
+        library_filename : string
+            Path to geneset library file. Library file must contain one header
+            line! The columns are: 1. Library ID, 2. Geneset ID, 3. Semicolon
+            separated list of gene ids.
+        from_enrichr : boolean
+            Whether geneset library shall be loaded from enrichr database or
+            not.
+        '''
+        if(from_enrichr):
+            # Retrieve genesets for each library and store in self.__genesets
+            query_string="/geneSetLibrary?mode=text&libraryName=%s"
+            response = requests.get(self.__enrichr_url + 
+                                    query_string % library_id)
+            if not response.ok:
+                    raise Exception('Error searching for terms')
+            for geneset_string in response.text.split("\n"):
+                geneset_list = geneset_string.split("\t")
+                geneset_id = geneset_list[0]
+                geneset = ";".join([gene.split(",")[0] for 
+                                    gene in geneset_list[2:]])
+                self.__genesets.loc[library_id+
+                                    "@"+
+                                    geneset_id, :] = [library_id,
+                                                      geneset_id,
+                                                      geneset]
+        else:
+            if(not library_filename is None):
+                library_file = open(library_filename, "r")
+                c = 0
+                for line in library_file:
+                    if(c == 0):
+                        c += 1
+                        continue
+                    split_line = line.rstrip().split("\t")
+                    lib_id = split_line[0]
+                    if(not(lib_id == library_id)):
+                        continue
+                    geneset_id = split_line[1]
+                    genelist = split_line[2]
+                    self.__genesets.loc[lib_id+
+                                        "@"+
+                                        geneset_id, :] = [lib_id,
+                                                          geneset_id,
+                                                          genelist]
+
+
+    # Enrichment Methods
+    def enrich(self,
+               method="all"):
+        '''Perform Enrichment Analysis.
+        Parameters
+        ----------
+        method : string
+            Statistical method used for enrichment analysis. Can be either of
+            fisher, hypergeometric, binomial, all.
+        '''
+        geneset_list_foreground_regions = [ set(geneset.split(";")) for
+                                        geneset in 
+                                        self.__annotated_foreground["GENESET"]]
+        geneset_list_background_regions = [ set(geneset.split(";")) for
+                                        geneset in 
+                                        self.__annotated_background["GENESET"]]
+
+        n_foreground = len(self.__annotated_foreground.index)
+        n_background = len(self.__annotated_background.index)
+
+        for library_id in list(set(self.__genesets.loc[:, "LIB.ID"])):
+            print("Calculate Enrichment for: "+library_id)
+            for geneset_id in self.__genesets[self.__genesets["LIB.ID"] == 
+                                          library_id].loc[:, "GENESET.ID"]:
+                if(not library_id+"@"+geneset_id in 
+                    set(self.__enrichment_results.index)):
+                    geneset = set(self.__genesets.loc[library_id+
+                                                      "@"+
+                                                      geneset_id,
+                                                      "GENE.LIST"].split(";"))
+                    n_foreground_in_geneset = self.__calculateGenesetOverlaps(
+                                            geneset_list_foreground_regions,
+                                            geneset)
+                    n_background_in_geneset = self.__calculateGenesetOverlaps(
+                                            geneset_list_background_regions,
+                                            geneset)
+
+                    p_val_fisher = None
+                    odds_fisher = None
+                    p_val_hyper = None
+                    p_val_binom = None
+                    if(method == "fisher"):
+                        n_foreground_not_in_geneset = (n_foreground-
+                                                       n_foreground_in_geneset)
+                        n_background_not_in_geneset = (n_background-
+                                                       n_background_in_geneset)
+                        ct = [ [n_foreground_in_geneset,
+                                n_foreground_not_in_geneset], 
+                               [n_background_in_geneset,
+                                n_background_not_in_geneset] ]
+                        odds_fisher, p_val_fisher = fisher_exact(ct, 
+                                                    alternative = "greater")
+                    elif(method == "hypergeometric"):
+                        M = n_background
+                        n = n_background_in_geneset
+                        N = n_foreground
+                        k = n_foreground_in_geneset
+                        p_val_hyper = 1.-hypergeom.cdf(k, M, n, N)
+                    elif(method == "binomial"):
+                        p = float(n_background_in_geneset)/float(n_background)
+                        x = n_foreground_in_geneset
+                        n = n_foreground
+                        p_val_binom = binom_test(x, 
+                                             n = n, 
+                                             p = p, 
+                                             alternative="greater")
+                    elif(method == "all"):
+                        # Fisher's Exact
+                        n_foreground_not_in_geneset = (n_foreground-
+                                                       n_foreground_in_geneset)
+                        n_background_not_in_geneset = (n_background-
+                                                       n_background_in_geneset)
+                        ct = [ [n_foreground_in_geneset,
+                                n_foreground_not_in_geneset], 
+                               [n_background_in_geneset,
+                                n_background_not_in_geneset] ]
+                        odds_fisher, p_val_fisher = fisher_exact(ct, 
+                                                    alternative = "greater")
+
+                        # Hypergeometric
+                        M = n_background
+                        n = n_background_in_geneset
+                        N = n_foreground
+                        k = n_foreground_in_geneset
+                        p_val_hyper = 1.-hypergeom.cdf(k, M, n, N)
+
+                        # Binomial
+                        p = float(n_background_in_geneset)/float(n_background)
+                        x = n_foreground_in_geneset
+                        n = n_foreground
+                        p_val_binom = binom_test(x, 
+                                             n = n, 
+                                             p = p, 
+                                             alternative="greater")
+                results = [library_id,
+                           geneset_id,
+                           n_foreground,
+                           n_background,
+                           n_foreground_in_geneset,
+                           n_background_in_geneset,
+                           p_val_fisher,
+                           None,
+                           odds_fisher,
+                           p_val_hyper,
+                           None,
+                           p_val_binom,
+                           None]
+                self.__enrichment_results.loc[
+                    library_id+"@"+geneset_id, :] = results
+
+            # Perform multiple testing correction
+            indices = self.__enrichment_results[
+                self.__enrichment_results["LIB.ID"] == library_id].index
+
+            if(method == "fisher" or method == "all"):
+                p_values = self.__enrichment_results.loc[indices, "p.FISHER"]
+                r, q_values, a_s, a_b = multipletests(p_values, 
+                                                      method = "fdr_bh")
+                self.__enrichment_results.loc[indices, "q.FISHER"] = q_values
+            if(method == "hypergeometric" or method == "all"):
+                p_values = self.__enrichment_results.loc[indices, 
+                                                         "p.HYPERGEOMETRIC"]
+                r, q_values, a_s, a_b = multipletests(p_values, 
+                                                      method = "fdr_bh")
+                self.__enrichment_results.loc[indices, 
+                                              "q.HYPERGEOMETRIC"] = q_values
+            if(method == "binomial" or method == "all"):
+                p_values = self.__enrichment_results.loc[indices, "p.BINOMIAL"]
+                r, q_values, a_s, a_b = multipletests(p_values, 
+                                                      method = "fdr_bh")
+                self.__enrichment_results.loc[indices, "q.BINOMIAL"] = q_values
 
     #################
     # Private Methods
@@ -240,20 +503,25 @@ class Enricher():
         self.__library_ids = [lib["libraryName"] for 
                               lib in lib_json["statistics"]]
 
-        # Retrieve genesets for each library and store in self.__genesets
-        for library_id in self.__library_ids[:3]:
-            print(library_id)
-            query_string="/geneSetLibrary?mode=text&libraryName=%s"
-            response = requests.get(self.__enrichr_url + 
-                                    query_string % library_id)
-            if not response.ok:
-                    raise Exception('Error searching for terms')
-            for geneset_string in response.text.split("\n"):
-                geneset_list = geneset_string.split("\t")
-                geneset_id = geneset_list[0]
-                geneset = ";".join(geneset_list[2:])
-                self.__genesets.loc[library_id+
-                                    "@"+
-                                    geneset_id, :] = [library_id,
-                                                      geneset_id,
-                                                      geneset]
+    def __calculateGenesetOverlaps(self,
+                                   region_associated_geneset_list,
+                                   geneset):
+        '''Calculate number of regions in geneset
+        Parameters
+        ----------
+        region_associated_geneset_list : list<set>
+            List of genesets the regions are associated with
+        geneset : set
+            Set of genes
+
+        Returns
+        -------
+        overlaps : int
+            Number of regions in geneset
+        '''
+        overlaps = 0
+        for region_associated_geneset in region_associated_geneset_list:
+            if(len(region_associated_geneset & geneset) > 0):
+                overlaps += 1
+        return overlaps
+
